842b9654e113c617cc8996ee80465036ad10fa87
Saiyedul Morsalin <mdsaiyedulmorsalin.code@gmail.com>
2024-03-23 21:22:23 +0600

    DSU and MST

---
+++ Week03/Practiceday01/bfs.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+
+int main(){
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<i<<j;
+        }
+    }
+    cout<<"Hello";
+    return 0;
+}
